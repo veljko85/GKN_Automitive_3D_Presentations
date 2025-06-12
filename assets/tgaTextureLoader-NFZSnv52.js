56eb04835686a4b62c497e7bcf94692213095e5b
Velibor Markovic <veljko85@users.noreply.github.com>
2025-06-12 09:12:01 +0200

    Add files via upload

---
+++ assets/tgaTextureLoader-NFZSnv52.js
@@ -0,0 +1,1 @@
+import{u as r,v as n}from"./orientationChengeChecker-BtJhBB0n.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{f as _TGATextureLoader};
