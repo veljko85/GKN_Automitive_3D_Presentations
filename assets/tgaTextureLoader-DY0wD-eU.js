db93056d6d5fe22df6af29ccf40f5eabe22fe26c
Velibor Markovic <veljko85@users.noreply.github.com>
2025-07-17 17:40:04 +0200

    Add files via upload

---
+++ assets/tgaTextureLoader-DY0wD-eU.js
@@ -0,0 +1,1 @@
+import{v as r,w as n}from"./orientationChengeChecker-BI50UZJ1.js";class f{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,o){const s=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),a=r(s);o(a.width,a.height,t.generateMipMaps,!1,()=>{n(t,s)})}}export{f as _TGATextureLoader};
